5efcf303e12b6a19197ce02eaf4c55b3d5ae2175
AlexandreKupfermunz <akupfermunz@icloud.com>
2026-06-10 23:31:35 +0200

    implemented predict and train methods

--- Perceptron.java
+++ Perceptron.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 /**
  * This class trains a simple perceptron.
  * The perceptron has two inputs, one bias, and one output.
@@ -8,9 +6,6 @@ public class Perceptron {
 
     static double learningRate = 0.1;
     static double[] weights = new double[3]; 
-    // weights[0] = input 1 weight
-    // weights[1] = input 2 weight
-    // weights[2] = bias weight
 
     public static void main(String[] args) {
         boolean verbose = false;
@@ -27,11 +22,11 @@ public class Perceptron {
         };
 
 
-        int [] OrTargets = { 0, 1, 1, 1 };
-        double [] OrWeights = new double[3];
+        int [] orTargets = { 0, 1, 1, 1 };
+        double [] orWeights = new double[3];
 
-        int [] AndTarget = { 0, 0, 0, 1 };
-        double [] AndWeights = new double[3];
+        int [] andTarget = { 0, 0, 0, 1 };
+        double [] andWeights = new double[3];
 
         // TODO 2: train perceptron until all inputs are classified correctly
 
@@ -49,29 +44,51 @@ public class Perceptron {
         weights[2] = Math.random()*2 - 1;
     }
 
-    // TODO 7: create predict()
-    static boolean predict(double[][] inputs, double[] weights){
+    static int predict(int[] inputs, double[] weights) {
+        double bias = 1.0;
+        double activation = inputs[0] * weights[0] + inputs[1] * weights[1] + bias * weights[2];
 
-
-        return false;
+        if (activation >= 0) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
-    // Should calculate:
-    // activation = input1*w1 + input2*w2 + bias*wbias
-    // If activation >= 0, return 1
-    // Else return 0
 
-    // TODO 8: create train()
-    // For each input:
-    // - predict output
-    // - calculate error = target - output
-    // - update weights if error is not 0
-    // Stop only when all outputs are correct
+    static int train(int[] targets, int[][] inputs, double[] weights, boolean verbose) {
 
-    // TODO 9: create updateWeights()
-    // Rule:
-    // new weight = old weight + learningRate * error * input
+        boolean allCorrect = false;
+        int iterations = 0;
 
-    // TODO 10: create printWeights()
+        while (!allCorrect) {
 
-    // TODO 11: create optional verbose printing
+            allCorrect = true;
+            iterations++;
+
+            for (int i = 0; i < inputs.length; i++) {
+
+                int output = predict(inputs[i], weights);
+                int error = targets[i] - output;
+
+                if (error != 0) {
+                    allCorrect = false;
+
+                    weights[0] = weights[0] + learningRate * error * inputs[i][0];
+                    weights[1] = weights[1] + learningRate * error * inputs[i][1];
+                    weights[2] = weights[2] + learningRate * error; // bias update
+
+                    if (verbose) {
+                        System.out.println("Input: " + inputs[i][0] + ", " + inputs[i][1]);
+                        System.out.println("Target: " + targets[i]);
+                        System.out.println("Output: " + output);
+                        System.out.println("Error: " + error);
+                        System.out.println("Weights: " + weights[0] + ", " + weights[1] + ", " + weights[2]);
+                        System.out.println();
+                    }
+                }
+            }
+        }
+
+        return iterations;
+    }
 }
